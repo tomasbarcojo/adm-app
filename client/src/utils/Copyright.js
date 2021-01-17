25f81d5ce03498ffff4a8985ee4a7e8e6d4363c4
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-01-17 17:03:29 -0300

    Home page

---
+++ client/src/utils/Copyright.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    copy: {
+        position: "relative",
+        color: 'black'
+    }
+}))
+
+export default function Copyright() {
+    const classes = useStyles()
+    return (
+        <Typography variant="body2" className={classes.copy} align="center">
+            {'Copyright © '}
+            <Link className={classes.copy} to="/">
+                Activa SRL
+      </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
