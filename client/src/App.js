cb22a91064bb6144a2810c12fa82811e2881ec13
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-01-17 17:39:53 -0300

    Login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/Home/Home'
+import LogIn from './components/Login/Login'
 
 const App = () => {
 
   return (
     <Router>
         <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={LogIn} />
         </Switch>
     </Router>
   );
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
@@ -11,16 +10,10 @@ import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
 import Copyright from '../../utils/Copyright'
+import '../../App.css'
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
@@ -38,9 +31,8 @@ export default function SignIn() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
+    <div className='LoginPageOuterContainer'>
+      <div className='LoginPageForm'>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
         </Avatar>
@@ -54,7 +46,7 @@ export default function SignIn() {
             required
             fullWidth
             id="email"
-            label="Email"
+            label="Email o Usuario"
             name="email"
             autoComplete="email"
             autoFocus
@@ -100,6 +92,6 @@ export default function SignIn() {
       <Box mt={8}>
         <Copyright />
       </Box>
-    </Container>
+    </div>
   );
 }
