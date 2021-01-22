4bf63cb42a8d93effad0e58bb5c6a5632ddf3898
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-01-22 10:18:07 -0300

    Working on register page, add redux when submit

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
@@ -6,11 +6,10 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
-import Copyright from '../../utils/Copyright'
+import { useDispatch } from 'react-redux'
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -34,6 +33,21 @@ const useStyles = makeStyles((theme) => ({
 
 export default function SignUp() {
   const classes = useStyles();
+  const dispatch = useDispatch()
+  const [data, setData] = useState({
+    firstName: '',
+    lastName: '',
+    username: '',
+    password: '',
+  })
+
+  const handleChange = (event) => {
+    setData({...data, [event.target.name]: event.target.value})
+  }
+
+  const handleSubmit = () => {
+    dispatch() //add user
+  }
 
   return (
     <div className='LoginPageOuterContainer'>
@@ -44,7 +58,7 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Registro
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -54,8 +68,9 @@ export default function SignUp() {
                 required
                 fullWidth
                 id="firstName"
-                label="First Name"
+                label="Nombre"
                 autoFocus
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -64,9 +79,10 @@ export default function SignUp() {
                 required
                 fullWidth
                 id="lastName"
-                label="Last Name"
+                label="Apellido"
                 name="lastName"
                 autoComplete="lname"
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -75,9 +91,10 @@ export default function SignUp() {
                 required
                 fullWidth
                 id="email"
-                label="Email Address"
-                name="email"
+                label="Usuario"
+                name="user"
                 autoComplete="email"
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
@@ -86,16 +103,17 @@ export default function SignUp() {
                 required
                 fullWidth
                 name="password"
-                label="Password"
+                label="Contraseña"
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                onChange={handleChange}
               />
             </Grid>
             <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
+                label="Aca se puede agregar una opcion por ej: RECIBIR MAILS CON NOVEDADES"
               />
             </Grid>
           </Grid>
@@ -106,7 +124,7 @@ export default function SignUp() {
             color="primary"
             className={classes.submit}
           >
-            Sign Up
+            Registrarse
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
@@ -117,9 +135,6 @@ export default function SignUp() {
           </Grid>
         </form>
       </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
     </div>
   );
 }
