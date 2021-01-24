9683689f92b75fb2efa2d3e7a8545f4c66322a0d
tomasbarcojo <tomasbarcojo@gmail.com>
2021-01-24 11:52:34 -0300

    Added dashboard

--- client/src/routes/App.js
+++ client/src/routes/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import PrivateRoutes from '../auth/PrivateRoutes'
 import LogIn from '../components/Login/Login'
 import Register from '../components/Register/Register'
+import Dashboard from '../components/Dashboard/Dashboard'
 
 const App = () => {
 
@@ -11,6 +12,7 @@ const App = () => {
             <Route exact path="/" component={LogIn} />
             <Route exact path="/register" component={Register} />
             {/* <PrivateRoutes exact path="/privateroute" component={Register} /> */}
+            <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
     </Router>
   );
