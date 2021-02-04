ca0ed437940f89b5b313090e8c9d6e9954328a23
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-02-04 12:03:24 -0300

    Charts

--- client/src/variables/charts.js
+++ client/src/variables/charts.js
@@ -17,7 +17,7 @@ var delays2 = 80,
 
 const dailySalesChart = {
   data: {
-    labels: ["M", "T", "W", "T", "F", "S", "S"],
+    labels: ["L", "M", "M", "J", "V", "S", "D"],
     series: [[12, 17, 7, 17, 23, 18, 38]]
   },
   options: {
@@ -25,7 +25,7 @@ const dailySalesChart = {
       tension: 0
     }),
     low: 0,
-    high: 50, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    high: 50,
     chartPadding: {
       top: 0,
       right: 0,
@@ -144,7 +144,7 @@ const completedTasksChart = {
       tension: 0
     }),
     low: 0,
-    high: 1000, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+    high: 1000,
     chartPadding: {
       top: 0,
       right: 0,
