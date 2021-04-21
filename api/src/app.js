2fd948f56af912bed49d1bf29e95e48378bb9ae5
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-04-21 14:02:50 -0300

    Added cors

--- api/src/app.js
+++ api/src/app.js
@@ -8,6 +8,7 @@ const cors = require('cors')
 
 server.name = "API";
 
+server.use(cors())
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
@@ -16,19 +17,9 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "*");
-  // res.header(
-	// 	'Access-Control-Allow-Headers',
-	// 	'Origin, X-Requested-With, Content-Type, Accept'
-	// )
   res.header("Access-Control-Allow-Methods", "*");
-  // res.header(
-	// 	'Access-Control-Allow-Methods',
-	// 	'GET, POST, OPTIONS, PUT, DELETE, PATCH'
-	// );
   next();
 });
-
-server.use(cors())
 
 server.use("/", routes);
 
