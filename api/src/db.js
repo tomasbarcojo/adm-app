53c37cb4e6cd9996ac105027bef1b208b75336ce
tomasbarcojo <tomasbarcojo@gmail.com>
2021-05-07 16:06:39 -0300

    fix filenames

--- api/src/db.js
+++ api/src/db.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const path = require('path');
 const {  DB_USER, DB_PASSWORD, DB_HOST,} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/activa`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -29,11 +29,13 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 
-const { User, Supplier, Client, PriceList } = sequelize.models;
+const { User, Supplier, Client, Pricelist } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Relaciones:
 
+Pricelist.hasMany(Client);
+Client.belongsTo(Pricelist);
 
 
 
