38a3511c8934042332a721f0c6d3c95ba75362db
Tomas Barcojo <tomasbarcojo@gmail.com>
2021-07-27 17:40:17 -0300

    Fixed products card styles and other changes

--- client/src/components/ProductCard/ProductCard.js
+++ client/src/components/ProductCard/ProductCard.js
@@ -19,13 +19,17 @@ import Image from '../../images/sayri.png'
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    maxWidth: 250,
-    // maxHeight: 320,
+    display: 'flex',
+    flexDirection: 'column',
+    width: 250,
     margin: '10px'
+  },
+  title: {
+    height: '60px'
   },
   media: {
     height: 0,
-    paddingTop: '100%', // 16:9
+    paddingTop: '200px', // 16:9
     objectFit: 'contain',
   },
   expand: {
@@ -46,6 +50,7 @@ const useStyles = makeStyles((theme) => ({
 export default function RecipeReviewCard({ props }) {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const string = 'SMOKING REGULAR (Diam 8mm / Long 15mm)'
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
@@ -53,75 +58,19 @@ export default function RecipeReviewCard({ props }) {
 
   return (
     <Card className={classes.root}>
-      <CardHeader
-        // avatar={
-        //   <Avatar aria-label="recipe" className={classes.avatar}>
-        //     R
-        //   </Avatar>
-        // }
-        // action={
-        //   <IconButton aria-label="settings">
-        //     <MoreVertIcon />
-        //   </IconButton>
-        // }
-        title={props.articleName}
-      // subheader="September 14, 2016"
-      />
       <CardMedia
         className={classes.media}
-        image={Image}
+        image={props.image}
         title={props.articleName}
       />
       <CardContent>
+        <Typography className={classes.title} gutterBottom variant="h6">
+          {props.articleName.length > 32 ? props.articleName.substring(0, 32) + '...' : props.articleName}
+        </Typography>
         <Typography variant="h5" color="textSecondary" component="p">
-          $ {props.price}
+          {props.price.toLocaleString('en-US', {style: 'currency',currency: 'USD'})}
         </Typography>
       </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <IconButton
-          className={clsx(classes.expand, {
-            [classes.expandOpen]: expanded,
-          })}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </IconButton>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Method:</Typography>
-          <Typography paragraph>
-            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-            minutes.
-          </Typography>
-          <Typography paragraph>
-            Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-            heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-            browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
-            and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
-            pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
-            saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-          </Typography>
-          <Typography paragraph>
-            Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-            without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
-            medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
-            again without stirring, until mussels have opened and rice is just tender, 5 to 7
-            minutes more. (Discard any mussels that don’t open.)
-          </Typography>
-          <Typography>
-            Set aside off of the heat to let rest for 10 minutes, and then serve.
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
