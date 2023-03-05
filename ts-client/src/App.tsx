beced0d32d73c6907cdb2c669660d70bb058df03
Tomas Barcojo <tomasbarcojo@gmail.com>
2023-03-05 17:50:33 -0300

    fix: index

--- ts-client/src/App.tsx
+++ ts-client/src/App.tsx
@@ -1,20 +1,9 @@
 import React from 'react';
 
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import {} from 'redux-persist/lib/integration/react';
-
 import Home from './pages/Home';
-import store, { persistor } from './redux/store';
 
 const App: React.FC = () => {
-  return (
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <Home />
-      </PersistGate>
-    </Provider>
-  );
+  return <Home />;
 };
 
 export default App;
--- ts-client/src/index.tsx
+++ ts-client/src/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from 'redux/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -7,9 +10,13 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
